1af95593441400758d257b77f34d88e7f994b39b
Jimothy <240087026+titandrive@users.noreply.github.com>
2026-06-14 08:12:35 -0700

    Revert "Rename plugin fullname to Bluetooth Configurator"
    This reverts commit e7a2cff3f75ab0426248080d7d4b7d659dc6e84b.

--- bluetoothconfigurator.koplugin/_meta.lua
+++ bluetoothconfigurator.koplugin/_meta.lua
@@ -1,6 +1,6 @@
 return {
     name = "bluetoothconfigurator",
-    fullname = "Bluetooth Configurator",
+    fullname = "Bluetooth Page Turner",
     description = "Map Bluetooth controller buttons to reader actions",
     version = 1,
 }
